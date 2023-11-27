800db28d2d5b467717247a9ee5a68a89d11c6442
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-27 16:42:04 +0530

    question and its answers added

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,19 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
 
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -59,6 +71,18 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 // The user picked true.
+
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer == true) {
+                  print('Correct !!!');
+                } else {
+                  print('Incorrect !!!');
+                }
+
+                setState(() {
+                  questionNumber++;
+                });
               },
               child: Text(
                 'True',
@@ -78,6 +102,18 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 // The user picked false.
+
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer == false) {
+                  print('Correct !!!');
+                } else {
+                  print('Incorrect !!!');
+                }
+
+                setState(() {
+                  questionNumber++;
+                });
               },
               child: Text(
                 'False',
